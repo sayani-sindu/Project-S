4abe1e5fbc9a26d036d483d3ef756ef57943c162
Raman0925 <ramankumarkannaujiya@gmail.com>
2024-11-17 10:08:50 +0530

    Added healthcheck routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+//import Routes
+const healthCheckRoutes = require("./routes/healthcheck.routes");
+
+app.use("api/v1/healthcheck", healthCheckRoutes);
+
 app.listen(3000, () => {
   console.log("Server Started");
 });
