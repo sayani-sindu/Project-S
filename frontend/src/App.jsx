0cac433e3d72397cada061204695692f6849ae13
Raman0925 <ramankumarkannaujiya@gmail.com>
2024-12-06 22:06:01 +0530

    Added Routes in Reacts for Routes Managment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import Routes from './Router/Routes.jsx'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
+
     <>
+    <Routes  />
+
       
     </>
   )
---
+++ frontend/src/Router/Routes.jsx
@@ -0,0 +1,10 @@
+import { Routes,Route } from "react-router-dom";
+const Routes = () =>{
+    return(<>
+    <Routes>
+        <Route path="/" element={}/>
+    </Routes>
+    
+    </>)
+}
+export default Routes;
