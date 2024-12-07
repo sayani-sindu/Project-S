7ee8343a72304d30c76bfa9ee343328f438db85b
Raman0925 <ramankumarkannaujiya@gmail.com>
2024-12-07 21:18:19 +0530

    Fix the component

--- frontend/src/Router/RoutersContainer.jsx
+++ frontend/src/Router/RoutersContainer.jsx
@@ -1,20 +1,18 @@
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
-import SignOut from "../pages/SignOut";
+import SignUp from "../pages/SignUp";
 import PageNotFound from "../Components/PageNotFound";
-const RoutersContainer = () =>{
-    
-    return(<>
-    <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/signin" element={<SignIn/>}/>
-        <Route path="/signout" element={<SignOut/>}/>
-        <Route path="*" element={<PageNotFound/>}/>
-        
-
-
-    </Routes>
-    </>)
-}
+const RoutersContainer = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+};
 export default RoutersContainer;
