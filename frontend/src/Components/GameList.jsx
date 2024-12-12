b3fcd869e7a1445e60c383888ac4799612a3fab7
Raman0925 <ramankumarkannaujiya@gmail.com>
2024-12-12 22:34:36 +0530

    Added some task

--- frontend/src/Components/GameList.jsx
+++ frontend/src/Components/GameList.jsx
@@ -1,6 +1,9 @@
 const GameList = () => {
     return(<>
-     
+     {/* Template 
+       Card which should have image and a link[Title] [Card and Link Component from MUI JOY]
+
+     */}
     </>)
 }
 export default GameList;
