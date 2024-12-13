2b50d944f4e518c297a9879bfd5ba4d87d0dd383
Raman0925 <ramankumarkannaujiya@gmail.com>
2024-12-13 20:20:33 +0530

    Converted Code in Joyui

--- frontend/src/Components/GameList.jsx
+++ frontend/src/Components/GameList.jsx
@@ -1,31 +1,32 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
+import * as React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  AspectRatio,
+  CardOverflow,
+} from "@mui/joy";
+
 const GameList = () => {
-    return(<>
-       <Card sx={{ maxWidth: 345 }}>
-       <CardActionArea>
-         <CardMedia
-           component="img"
-           height="140"
-           image="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.discoverycommons.com%2Fsenior-living-blog%2Fthe-advantages-of-playing-trivia-in-your-golden-years%2
-           F&psig=AOvVaw2PGEZGeHJ1MT6uHLfo81Xz&ust=1734178820478000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMD36N3dpIoDFQAAAAAdAAAAABAE"
-           alt="Trivia"
-         />
-         <CardContent>
-           <Typography gutterBottom variant="h5" component="div">
-             Trivia Game
-           </Typography>
-           <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              A trivia game is a competition where players answer questions about interesting but
-              unimportant facts on a variety of topics.
-           </Typography>
-         </CardContent>
-       </CardActionArea>
-     </Card>
-    </>)
-}
+  return (
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardOverflow>
+          <AspectRatio sx={{ minWidth: 200 }}>
+            <img src="" srcSet=" 2x" loading="lazy" alt="" />
+          </AspectRatio>
+        </CardOverflow>
+
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Trivia Game
+          </Typography>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            Create Quizzes and Play with your friends
+          </Typography>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
 export default GameList;
