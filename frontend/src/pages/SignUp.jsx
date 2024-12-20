1484c93fc58e796d65eb0cbe2b5774dda7670e89
Raman0925 <ramankumarkannaujiya@gmail.com>
2024-12-20 22:38:15 +0530

    Data

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -6,6 +6,7 @@ const SignInForm = () => {
   return (
     <>
       <div className="flex justify-center items-center min-h-screen bg-gray-100 ">
+        Terms and Conditions apply
         <SignUpForm />
       </div>
     </>
